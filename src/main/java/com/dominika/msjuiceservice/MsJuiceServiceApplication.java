66e5e40070fec85830046810e5028533e835987b
dgoraj <dgoraj@deloittece.com>
2023-05-17 14:01:47 +0200

    initial structure

---
+++ src/main/java/com/dominika/msjuiceservice/MsJuiceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dominika.msjuiceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsJuiceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsJuiceServiceApplication.class, args);
+	}
+
+}
