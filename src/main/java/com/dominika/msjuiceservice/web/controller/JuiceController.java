c752c9dac842e7c330871d66ce397c90490c8de1
dgoraj <dgoraj@deloittece.com>
2023-05-17 14:16:37 +0200

    added controller

---
+++ src/main/java/com/dominika/msjuiceservice/web/controller/JuiceController.java
@@ -0,0 +1,30 @@
+package com.dominika.msjuiceservice.web.controller;
+
+import java.util.UUID;
+
+import com.dominika.msjuiceservice.web.model.JuiceDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/v1/juice")
+@RestController
+public class JuiceController {
+
+    @GetMapping("/{juiceId}")
+    public ResponseEntity<JuiceDto> getJuiceById(@PathVariable UUID juiceId){
+        //TODO impl
+        return new ResponseEntity<>(JuiceDto.builder().build(), HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity saveNewJuice (@RequestBody JuiceDto juiceDto){
+        //TODO impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{juiceid}")
+    public ResponseEntity updateJuice(@PathVariable UUID juiceId){
+        //TODO impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
