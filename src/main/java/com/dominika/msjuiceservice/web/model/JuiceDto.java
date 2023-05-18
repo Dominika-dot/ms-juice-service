e2f4ca35c48df09dd750296c3bbbb1e263fcb4e0
dgoraj <dgoraj@deloittece.com>
2023-05-18 20:13:32 +0200

    validations for JuiceDto

--- src/main/java/com/dominika/msjuiceservice/web/model/JuiceDto.java
+++ src/main/java/com/dominika/msjuiceservice/web/model/JuiceDto.java
@@ -4,6 +4,10 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,13 +19,23 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class JuiceDto {
 
+    @Null
     private UUID id;
+    @Null
     private Integer version;
+    @Null //read-only to the client
     private OffsetDateTime createdDate;
+    @Null
     private OffsetDateTime lastModifiedDate;
-    private String beerName;
+    @NotBlank
+    private String juiceName;
+    @NotBlank
     private JuiceStyleEnum juiceStyle;
+    @Positive
+    @NotNull
     private Long upc;
+    @Positive
+    @NotNull
     private BigDecimal price;
     private Integer quantityOnHand;
 }
