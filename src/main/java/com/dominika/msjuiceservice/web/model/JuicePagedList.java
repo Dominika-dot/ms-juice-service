18f0ba18e8e153213a2554e1ebe557c508b5e7d9
dgoraj <dgoraj@deloittece.com>
2023-05-22 15:32:01 +0200

    added jsoncreator example

--- src/main/java/com/dominika/msjuiceservice/web/model/JuicePagedList.java
+++ src/main/java/com/dominika/msjuiceservice/web/model/JuicePagedList.java
@@ -2,10 +2,29 @@ package com.dominika.msjuiceservice.web.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 public class JuicePagedList extends PageImpl<JuiceDto> {
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public JuicePagedList(@JsonProperty("content") List<JuiceDto> content,
+                          @JsonProperty("number") int number,
+                          @JsonProperty("size") int size,
+                          @JsonProperty("totalElements") long totalElements,
+                          @JsonProperty("pageable") JsonNode pageable,
+                          @JsonProperty("last") boolean last,
+                          @JsonProperty("totalPages") int totalPages,
+                          @JsonProperty("sort") JsonNode sort,
+                          @JsonProperty("first") boolean first,
+                          @JsonProperty("numberOfElements") int numberOfElements){
+        super(content, PageRequest.of(number,size), totalElements);
+    }
+
     public JuicePagedList(final List<JuiceDto> content, final Pageable pageable, final long total) {
         super(content, pageable, total);
     }
